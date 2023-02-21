ddee7f5d6bbf58900d2ff4d2f50f4dadeeb8757b
Arrue Marianne <5608@holbertonstudents.com>
2023-02-21 00:51:24 -0800

    test function save and call variable storage

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 """
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -44,6 +45,7 @@ class BaseModel:
             # first initialization : creates and updated time = now
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -61,6 +63,7 @@ class BaseModel:
             update the public instance attribute updated_at
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
