b33dde54f047e0323792d5d811af9bb415ff1d51
Arrue Marianne <5608@holbertonstudents.com>
2023-02-22 02:59:03 -0800

    add class Review and Place

---
+++ models/review.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+    Class Review inherit from BaseModel
+"""
+import models
+from models.base_model import BaseModel
+import models.engine.file_storage
+
+
+class Review(BaseModel):
+    """
+        Class Review
+
+        ATTRIBUTS
+        ============
+            place_id: public, place's id -> Place.id
+
+            user_id: public, user's id -> User.id
+
+            text: public, text of review
+
+            **kwargs: **kwargs of class BaseModel
+
+    """
+
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        models.storage.save()
+        models.storage.new(self)
