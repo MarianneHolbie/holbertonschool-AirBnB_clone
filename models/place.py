8fc53d2727e65e7d20bdfe8efc9e64d97d2cd064
Arrue Marianne <5608@holbertonstudents.com>
2023-02-22 03:03:27 -0800

    correct import of other subclass

--- models/place.py
+++ models/place.py
@@ -6,6 +6,8 @@ import models
 from models.base_model import BaseModel
 from models.user import User
 from models.amenity import Amenity
+from models.city import City
+from models.state import State
 import models.engine.file_storage
 
 
