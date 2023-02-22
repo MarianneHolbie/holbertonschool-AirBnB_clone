92e3525962ccc14d44fa6044b0b0b83c8c922beb
Arrue Marianne <5608@holbertonstudents.com>
2023-02-22 04:29:05 -0800

    remove all import except BaseModel to suppress conflict circular import

--- models/place.py
+++ models/place.py
@@ -2,13 +2,7 @@
 """
     Class Place inherit from BaseModel
 """
-import models
 from models.base_model import BaseModel
-from models.user import User
-from models.amenity import Amenity
-from models.city import City
-from models.state import State
-import models.engine.file_storage
 
 
 class Place(BaseModel):
